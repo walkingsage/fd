f12662f2a1f356a93f32412c6271dffa3a1df97d
walkingsage <walkingsage@mail.ru>
2021-08-21 16:12:34 +0300

    1.7

--- js/script.js
+++ js/script.js
@@ -8,8 +8,27 @@ terms = document.querySelector('.terms'),
 exitBtn = document.getElementById('exit'),
 aurem = document.querySelectorAll('.aurem');
 
-let popupOpen = false,
-cookieAcceps = false;
+let popupOpen = false;
+
+function checkCookies(){
+    let cookieDate = localStorage.getItem('cookieDate');
+    let cookieNotification = document.getElementById('cookie_notification');
+    let cookieBtn = cookieNotification.querySelector('.cookie_accept');
+
+    // Если записи про кукисы нет или она просрочена на 1 год, то показываем информацию про кукисы
+    if( !cookieDate || (+cookieDate + 31536000000) < Date.now() ){
+        cookieNotification.classList.add('show');
+        console.log("no cuckie");
+    }
+
+    // При клике на кнопку, в локальное хранилище записывается текущая дата в системе UNIX
+    cookieBtn.addEventListener('click', function(){
+        localStorage.setItem( 'cookieDate', Date.now() );
+        cookieNotification.classList.remove('show');
+    })
+}
+
+checkCookies();
 
 $(document).ready(function () {
     let userheight = window.innerHeight;
